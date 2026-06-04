c0d1c1cfae00781f8ec8f3b9ff5996fb9efcdfd4
Mona-Thakur-10 <thakurmona820@gmail.com>
2026-06-05 02:16:40 +0530

    Enhance chatbot responses and add greetings

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,19 @@
+print("AI Chatbot Started!")
 responses = {"how are you":"I am Fine!",
              "hy":"hie there!",
              "hello":"hie there!",
-             "help":"How can I help?",
+             "good morning":"Good Morning!",
+             "good evening":"Good Evening!",
+             "help":"How can I help ?",
              "bye":"Good Bye!",
              "thanks":"Wlecome!"
             }
+
 while True:
     user = input("You: ").lower().strip()
     if user == "exit":
         print("Bot: Bye!")
         break
     
-    reply = responses.get(user,"I dont Understand..")
+    reply = responses.get(user,"Sorry,I dont Understand that.. \nPlease try another Question.")
     print("Bot: ", reply) 
